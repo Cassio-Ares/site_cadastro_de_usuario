7f63a0b4219bc7a935f7c5733a3a8dcd5ea748cf
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-06-24 19:31:17 -0300

    visualizar e ocultar senha

---
+++ js/eye_password.js
@@ -0,0 +1,35 @@
+// window.addEventListener('load', ()=>{
+//     document.querySelector(".fa-solid").addEventListener('click', ()=>{
+//             document.querySelector(".fa-solid").classList.toggle("fa-eye-slash")
+//             document.querySelector(".fa-solid").classList.toggle("fa-eye")
+//     })
+// })
+
+// window.addEventListener("load", () => {
+//   const icons = document.querySelectorAll(".fa-solid");
+//   const password = document.getElementById("password");
+//   const passwordConfirm = document.getElementById("password_confirmation");
+
+//   icons.forEach((icon) => {
+//     icon.addEventListener("click", () => {
+//       icon.classList.toggle("fa-eye-slash");
+//       icon.classList.toggle("fa-eye");
+
+//       if(password.type === "password"){
+//          password.type = "text";
+//       }else{
+//         password.type = "password"
+//       }
+
+//       if(passwordConfirm.type === "password"){
+//         passwordConfirm.type = "text";
+//      }else{
+//        passwordConfirm.type = "password"
+//      }
+//     });
+//   });
+// });
+
+// function para fazer o toggle com event.srcElement
+
+
